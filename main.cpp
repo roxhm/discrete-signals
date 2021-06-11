310d1c6b5d2f980f94ca9a82766118aecbbaad1a
roxhm <roxy.hm98@gmail.com>
2021-06-10 20:05:04 -0500

    Agrega ops. diezmación, interpolación, convolución.

--- main.cpp
+++ main.cpp
@@ -65,9 +65,9 @@ class Secuencia
 	{
 		int i = min(menor_indice(), b.menor_indice()); 
 		int j = max(mayor_indice(), b.mayor_indice()); 
-		int tamaño = j - i + 1; 
+		int tamano = j - i + 1; 
 
-		vector<float> muestras_suma(tamaño); 
+		vector<float> muestras_suma(tamano); 
 		int indice_0_suma = max(indice_0, b.indice_0); 
 		
 		Secuencia resultado(indice_0_suma, muestras_suma); 
@@ -84,9 +84,9 @@ class Secuencia
 	{
 		int i = min(menor_indice(), b.menor_indice()); 
 		int j = max(mayor_indice(), b.mayor_indice()); 
-		int tamaño = j - i + 1; 
+		int tamano = j - i + 1; 
 
-		vector<float> muestras_suma(tamaño); 
+		vector<float> muestras_suma(tamano); 
 		int indice_0_suma = max(indice_0, b.indice_0); 
 		
 		Secuencia resultado(indice_0_suma, muestras_suma); 
@@ -103,9 +103,9 @@ class Secuencia
 	{
 		int i = max(menor_indice(), b.menor_indice()); 
 		int j = min(mayor_indice(), b.mayor_indice()); 
-		int tamaño = j - i + 1; 
+		int tamano = j - i + 1; 
 
-		vector<float> muestras_producto(tamaño); 
+		vector<float> muestras_producto(tamano); 
 		int indice_0_producto = min(indice_0, b.indice_0); 
 
 		Secuencia resultado(indice_0_producto, muestras_producto); 
@@ -169,6 +169,87 @@ class Secuencia
 		}
 
 		
+	}
+
+	void diezmar(int k)
+	{
+		vector<float> nuevas_muestras;
+		int nuevo_indice_0 = -1; 	
+		for(int i = menor_indice(); i <= mayor_indice(); i++)
+		{
+			if(i % k == 0) 
+				nuevas_muestras.push_back((*this)[i]);
+		       	if(i == 0) 	
+				nuevo_indice_0 = nuevas_muestras.size() - 1; 
+		}
+		this -> muestras = nuevas_muestras;
+	       	this -> indice_0 = nuevo_indice_0; 	
+	}
+
+	void interpolacion_cero(int k)
+	{
+		vector<float> muestras_interpoladas;
+	        for(int i = menor_indice();  i <= mayor_indice(); i++) 
+		{
+			muestras_interpoladas.push_back((*this)[i]); 
+			if(i < mayor_indice())
+				for(int j = 0; j < k - 1; j++) 
+					muestras_interpoladas.push_back(0); 
+		}
+		this -> indice_0 *= k;
+		this -> muestras = muestras_interpoladas; 
+	}
+
+	void interpolacion_escalon(int k) 
+	{
+		vector<float> muestras_interpoladas;
+	        for(int i = menor_indice();  i <= mayor_indice(); i++) 
+		{
+			for(int j = 0; j < k; j++) 
+				muestras_interpoladas.push_back((*this)[i]); 
+		}
+		this -> indice_0 *= k;
+		this -> muestras = muestras_interpoladas; 
+	}
+
+	void interpolacion_lineal(int k)
+	{
+		vector<float> muestras_interpoladas;
+	        for(int i = menor_indice();  i <= mayor_indice(); i++) 
+		{
+			float adyacente_izquierdo = (*this)[i]; 
+			float adyacente_derecho = (*this)[i + 1];
+
+			float constante = (adyacente_derecho - adyacente_izquierdo) / k; 	
+			
+			for(int j = 0; j < k; j++) 
+				muestras_interpoladas.push_back(adyacente_izquierdo + j*constante); 
+		}
+		this -> indice_0 *= k;
+		this -> muestras = muestras_interpoladas; 
+	}
+
+	Secuencia convolucion(Secuencia b)
+	{
+		int minimo = menor_indice() + b.menor_indice(); 
+		int maximo = mayor_indice() + b.mayor_indice(); 
+		vector<float> muestras_convolucion; 
+		int nuevo_indice_0 = -1; 
+		for(int n = minimo; n <= maximo; n++)	
+		{
+			float muestra = 0;  
+			for(int k = menor_indice(); k <= mayor_indice(); k++) 
+			{
+				muestra += (*this)[k]*b[n - k]; 
+			}
+			muestras_convolucion.push_back(muestra); 
+			if(n == 0) 
+				nuevo_indice_0 = muestras_convolucion.size() - 1; 
+
+		}
+		Secuencia resultado(nuevo_indice_0, muestras_convolucion); 
+
+		return resultado;
 	}
 
 	void imprimir_secuencia_txt(string nombre_archivo)
@@ -338,8 +419,9 @@ int main(int num_argumentos, char* argumentos[])
 					printf("\n\n\tSecuencia: "); 
 					getline(cin, secuencia_a); 
 					Secuencia a(secuencia_a);
-				       	printf("\tFactor de atenuacion (0 < k < 1): "); 
+				       	printf("\tFactor de atenuacion (k > 1): ");
 					scanf("%f", &factor_atenuacion); 
+				       	factor_atenuacion = 1 / factor_atenuacion;	
 					Secuencia resultado = a * factor_atenuacion; 
 					cout << "\n\t\t" << factor_atenuacion << resultado << "\n";
 				        break;	
@@ -383,10 +465,107 @@ int main(int num_argumentos, char* argumentos[])
 		}
 		case 'e':
 		{
-			break; 
+			printf("\n\tSeleccione:");
+			printf("\n\n\t\t1) Diezmacion"); 
+			printf("\n\t\t2) Interpolacion");
+
+			char opcion2; 
+			printf("\n\n\tOpcion: "); 
+			scanf("%c", &opcion2); 
+			getline(cin, vacia); 
+ 
+			switch(opcion2)
+			{
+				case '1':
+				{
+					string secuencia_a;
+					int factor_diezmacion;	
+					printf("\n\t\tDiezmacion");
+					printf("\n\n\tIngrese la secuencia en el formato [nombre={f(-2), f(-1), f(0)*, f(1), f(2)].\n\tEjemplo: a={-4, -2, 0*, 2, 4}"); 
+					printf("\n\n\tSecuencia: "); 
+					getline(cin, secuencia_a); 
+					Secuencia a(secuencia_a);
+					printf("\tFactor de diezmacion (k > 0): "); 
+					scanf("%d", &factor_diezmacion); 
+					a.diezmar(factor_diezmacion);  
+					cout << "\n\t\t" << a << "\n"; 
+					break;
+				}
+				case '2':
+				{
+					string secuencia_a;
+					printf("\n\t\tInterpolacion");
+				       	printf("\n\n\tIngrese la secuencia en el formato [nombre={f(-2), f(-1), f(0)*, f(1), f(2)].\n\tEjemplo: a={-4, -2, 0*, 2, 4}"); 
+					printf("\n\n\tSecuencia: "); 
+					getline(cin, secuencia_a); 
+					Secuencia a(secuencia_a);
+					int factor_interpolacion;  
+					printf("\tFactor de interpolacion (k > 0): "); 
+					scanf("%d", &factor_interpolacion);
+				       
+					printf("\n\n\tSeleccione: "); 	
+					printf("\n\n\t\t1) Interpolacion a cero"); 
+					printf("\n\t\t2) Interpolacion escalon");
+					printf("\n\t\t3) Interpolacion lineal");
+
+					char opcion3; 
+					printf("\n\n\tOpcion: "); 
+					scanf(" %c", &opcion3); 
+					getline(cin, vacia);
+
+					switch(opcion3)
+					{
+						case '1':
+						{
+							a.interpolacion_cero(factor_interpolacion); 
+							cout << "\n\t\t" << a << "\n"; 
+							break;
+						}
+						case '2':
+						{
+							a.interpolacion_escalon(factor_interpolacion); 
+							cout << "\n\t\t" << a << "\n"; 
+							break;
+						}
+						case '3': 
+						{
+							a.interpolacion_lineal(factor_interpolacion); 
+							cout << "\n\t\t" << a << "\n"; 
+							break; 
+						}
+						default: 
+						{
+							break; 
+						}
+					}	
+		 
+
+					break; 
+				}
+				default: 
+				{
+					printf("Opcion invalida\n"); 
+					break; 
+				}
+			}
+		 
 		}
 		case 'f': 
 		{
+			string secuencia_a, secuencia_b; 
+			printf("\n\t\tConvolucion");
+			printf("\n\n\tIngrese las secuencias en el formato [nombre={f(-2), f(-1), f(0)*, f(1), f(2)].\n\tEjemplo: a={-4, -2, 0*, 2, 4}"); 
+		       	printf("\n\n\tPrimera secuencia: "); 
+			getline(cin, secuencia_a);  
+		       	printf("\tSegunda secuencia: ");	
+			getline(cin, secuencia_b);
+
+			Secuencia a(secuencia_a); 
+			Secuencia b(secuencia_b);
+
+			Secuencia resultado = a.convolucion(b);  
+			cout << "\n\t\t" << a.nombre << " * " << b.nombre <<  resultado << "\n"; 
+
 			break; 
 		}
 		default: 
